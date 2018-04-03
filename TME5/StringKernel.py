8f7bf75987422a5584bf64bb26a74368e63c2bc2
samutamm <samutamm@gmail.com>
2018-04-03 23:11:18 +0200

    last commit

--- TME5/StringKernel.py
+++ TME5/StringKernel.py
@@ -11,7 +11,7 @@ def find_all(a_str, sub):
 
 class StringKernel:
     
-    def __init__(self,lambda_param=1, k=2):
+    def __init__(self,lambda_param=1, k=1):
         self.l = lambda_param
         self.k = k
     
@@ -25,17 +25,16 @@ class StringKernel:
     
     def K_prime(self,i, sx, t):
         if i == 0:
-            print('jou')
             return 1
         if min(len(sx)-1, len(t)) < i:
             return 0
         x = sx[-1]
         s = sx[:-1]
         total = self.l * self.K_prime(i, s,t)
-        for j in range(len(t)):
+        for j in range(1,len(t)):
             if t[j] == x:
                 power = len(t) - j + 2
-                total += self.K_prime(i-1, s, t[1:j-1]) * (self.l ** power)
+                total += self.K_prime(i-1, s, t[:j-1]) * (self.l ** power)
         return total
                           
     def K(self, i, sx, t):
@@ -44,23 +43,30 @@ class StringKernel:
         x = sx[-1]
         s = sx[:-1]
         total = self.l * self.K(i, s,t)
-        for j in range(len(t)):
+        for j in range(1,len(t)):
             if t[j] == x:
-                total += self.K_prime(i-1, s, t[1:j-1]) * (self.l ** 2)
+                total += self.K_prime(i-1, s, t[:j-1]) * (self.l ** 2)
         return total
     
-    def K_hat(self, s,t):
-        return self.K(self.k, s,t) / np.sqrt(self.K(self.k, s,s) * self.K(self.k, t,t)) 
+    def K_hat(self, s,t, diagonal_s, diagonal_t):
+        return self.K(self.k, s,t) / np.sqrt(diagonal_s * diagonal_t) 
                           
     
-    def build_kernel(self, vocabulary, lambda_param=1):    
-        def kernel(X1,X2):
-            N = X1.shape[0]
-            M = X2.shape[0]
-            result = np.zeros((N,M))
-            for i in range(N):
-                for j in range(M):
-                    result[i,j] = self.handle_one_example(X1[i],X2[j], vocabulary, lambda_param)
-            return result
-        
-        return kernel
+    def build_kernel(self, X1, X2):    
+        N = X1.shape[0]
+        M = X2.shape[0]
+        result = np.zeros((N,M))
+        similarite_diagonal = {}
+        similarite_diagonal[1] = {}
+        similarite_diagonal[2] = {}
+        for i in range(N):
+            similarite_diagonal[1][i] = self.K(self.k, X1[i],X1[i])
+        for j in range(M):
+            similarite_diagonal[2][j] = self.K(self.k, X2[j],X2[j])
+            
+        for i in range(N):
+            for j in range(M):
+                simi = similarite_diagonal[1][i]
+                simj = similarite_diagonal[2][j]
+                result[i,j] = self.K_hat(X1[i],X2[j], simi,simj)
+        return result
